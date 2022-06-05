61ee42e0c09e811effbf349cc551e91dbd0cc3c1
Antea <antea.ribaric@gmail.com>
2022-06-05 16:15:29 +0200

    lint changes

--- app/src/main/java/com/example/sofanba/ui/player/PlayerMatchesFragment.kt
+++ app/src/main/java/com/example/sofanba/ui/player/PlayerMatchesFragment.kt
@@ -3,5 +3,4 @@ package com.example.sofanba.ui.player
 import androidx.fragment.app.Fragment
 import com.example.sofanba.model.Player
 
-class PlayerMatchesFragment(val player: Player) : Fragment() {
-}
+class PlayerMatchesFragment(val player: Player) : Fragment()
--- app/src/main/java/com/example/sofanba/ui/player/PlayerStatisticsFragment.kt
+++ app/src/main/java/com/example/sofanba/ui/player/PlayerStatisticsFragment.kt
@@ -3,5 +3,4 @@ package com.example.sofanba.ui.player
 import androidx.fragment.app.Fragment
 import com.example.sofanba.model.Player
 
-class PlayerStatisticsFragment(val player: Player) : Fragment() {
-}
+class PlayerStatisticsFragment(val player: Player) : Fragment()
